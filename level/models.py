ada8c8e66873a6871402e45119d2f2b6ad4644eb
정성우 <jeongseongwoo@jeongseong-uui-MacBookAir.local>
2023-11-14 15:29:07 +0900

    관리자 페이지에서 파일업로드 항목 추가하기

--- level/models.py
+++ level/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Subject(models.Model):
     head_image = models.ImageField(upload_to='level/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='level/files/%Y/%m/%d/', blank=True)
     title = models.CharField(max_length=40)
     content = models.TextField()
     price = models.IntegerField(default=0)
