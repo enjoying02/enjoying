b0746ade1e794e7342e1ced0329ba2cc5f9541fc
정성우 <jeongseongwoo@jeongseong-uui-MacBookAir.local>
2023-11-14 15:10:06 +0900

    미디어폴더 만들고 어드민페이지에서 적용

--- level/models.py
+++ level/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class Subject(models.Model):
+    head_image = models.ImageField(upload_to='level/images/%Y/%m/%d/', blank=True)
     title = models.CharField(max_length=40)
     content = models.TextField()
     price = models.IntegerField(default=0)
     #author:추후 작성예정
+
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
