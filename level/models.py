5fc20fd6e7de12ce1b1018ed5d1348fe9c590302
정성우 <jeongseongwoo@jeongseong-uui-MacBookAir.local>
2023-11-14 16:44:40 +0900

    모듈화, 카테고리화 제외하고 다 완료했음

--- level/models.py
+++ level/models.py
@@ -7,7 +7,6 @@ class Subject(models.Model):
     title = models.CharField(max_length=40)
     content = models.TextField()
     price = models.IntegerField(default=0)
-    #author:추후 작성예정
 
 
     def __str__(self):
