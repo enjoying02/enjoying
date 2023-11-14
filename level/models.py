3c3329a0d86520a73d487006ed01241c211383e2
정성우 <jeongseongwoo@jeongseong-uui-MacBookAir.local>
2023-11-14 17:50:27 +0900

    작성자 추가하기

--- level/models.py
+++ level/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 class Subject(models.Model):
@@ -7,10 +8,11 @@ class Subject(models.Model):
     title = models.CharField(max_length=40)
     content = models.TextField()
     price = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/level/{self.pk}/'
