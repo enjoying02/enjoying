8a21e7e5aae783514b347d48444813b0523a71f3
정성우 <jeongseongwoo@jeongseong-uui-MacBookAir.local>
2023-11-14 18:08:12 +0900

    어드민페이지에 카테고리항목 만들기

--- level/admin.py
+++ level/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Subject
+from .models import Subject, Category
 
 admin.site.register(Subject)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
+admin.site.register(Category, CategoryAdmin)
--- level/models.py
+++ level/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Subject(models.Model):
     head_image = models.ImageField(upload_to='level/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='level/files/%Y/%m/%d/', blank=True)
@@ -9,6 +19,8 @@ class Subject(models.Model):
     content = models.TextField()
     price = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
 
     def __str__(self):
